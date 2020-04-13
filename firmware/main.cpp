8db691319322f06608604e6b192812f1c6be2888
Tim Schaake <tsharky@posteo.de>
2020-04-13 22:26:24 +0200

    Project changes
    * New compiler version
    * Project formatter config
    * Apply formatter on main.cpp
    * MIT license

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -15,61 +15,64 @@
 
 void initLED(void);
 
-int main(void){
-    bool mainloop = true;
-    uint32_t sysClock = 0;
-    uint8_t uartData = 0;
+int main(void) {
+   bool mainloop = true;
+   uint32_t sysClock = 0;
+   uint8_t uartData = 0;
 
-    // set sysClock to 120 MHz
-    sysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
+   // set sysClock to 120 MHz
+   sysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),
+                                 120000000);
 
-    // LED
-    initLED();
+   // LED
+   initLED();
 
-    // UART
-    cUART uart(2);
-    uart.init(sysClock, 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-    uart.enable();
+   // UART
+   cUART uart(2);
+   uart.init(sysClock, 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+   uart.enable();
 
-    // CAN
-    cCAN can(0);
-    can.init(sysClock, 500000);
-    can.enable();
+   // CAN
+   cCAN can(0);
+   can.init(sysClock, 500000);
+   can.enable();
 
+   // enable global interrupts
+   IntMasterEnable();
 
-    // enable global interrupts
-    IntMasterEnable();
+   // start main loop
+   uart.writeData("running...");
+   uart.endl();
 
-    // start main loop
-    uart.writeData("running...");
-    uart.endl();
+   // main loop
+   while (mainloop)
+   {
+      uart.transmitData();
 
-    // main loop
-    while(mainloop){
-        uart.transmitData();
+      if (uart.dataAvailable()) {
+         uart.readData(&uartData);
+         uart.writeData(uartData);
+      }
+   }
 
-        if(uart.dataAvailable()){
-            uart.readData(&uartData);
-            uart.writeData(uartData);
-        }
-    }
-
-    return 0;
+   return 0;
 }
 
-void initLED(void){
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION));
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
+void initLED(void) {
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+   while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
+      ;
+   GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0);
+   GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_1);
 
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+   while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+      ;
+   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0);
+   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
 
-    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
-    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4);
+   GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+   GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
+   GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0);
+   GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4);
 }
